d9c33fbdfe4c24ab7478548642dbb72d1270e669
Ayush Sharma <47763441+AyushSharma21@users.noreply.github.com>
2021-01-11 05:13:06 -0500

    Role Commands added

--- PinguBot.py
+++ PinguBot.py
@@ -11,6 +11,10 @@ async def on_ready():
 async def on_command_error(ctx, error):
     if(isinstance(error, commands.MissingRequiredArgument)):
         await ctx.send("Missing argument!")
+    elif(isinstance(error, commands.RoleNotFound)):
+        await ctx.send("That role was not found on the server.")
+    elif(isinstance(error, commands.MemberNotFound)):
+        await ctx.send("That person was not found on the server.")
     else:
         raise error
     
@@ -23,7 +27,6 @@ async def clearMessage(ctx, number):
         if (convertNumberInt <= 0 or convertNumberInt > 100):
             await ctx.send("Must enter a number between 1 and 100 (inclusive).")
         else:
-            print("triggered")
             await ctx.message.delete()
             deletedCount = await ctx.channel.purge(limit = convertNumberInt)
             await ctx.send(f"Got rid of {len(deletedCount)} messages. ", delete_after=5)
@@ -31,6 +34,19 @@ async def clearMessage(ctx, number):
         await ctx.send("Invalid Argument")
         return
 
+@client.command()
+@bot_has_permissions(manage_roles=True)
+@has_permissions(manage_roles=True)
+async def addRole(ctx, role: discord.Role, user: discord.Member):
+    await user.add_roles(role)
+    await ctx.send(f"Granted roll {role.mention} to {user.mention}")
 
+@client.command()
+@bot_has_permissions(manage_roles=True)
+@has_permissions(manage_roles=True)
+async def removeRole(ctx, role: discord.Role, user: discord.Member):
+    await user.remove_roles(role)
+    await ctx.send(f"Removed roll {role.mention} from {user.mention}")
+    
 
-client.run('') //Add Your token here.
+client.run('') //Add your Token here.
